73b266c75aef741bbf9eb5126b2fbe7acbe42111
RoHiT-48566 <m.rohit48566@gmail.com>
2024-11-15 23:13:35 +0530

    quicksort-added

--- client/src/pages/AlgoMapping.jsx
+++ client/src/pages/AlgoMapping.jsx
@@ -8,9 +8,13 @@ const algoMapping = {
     import("../algorithms/BinarySearch/BinarySearch.jsx")
   ),
   "Bubble Sort": lazy(() => import("../algorithms/BubbleSort/BubbleSort.jsx")),
-  "Selection Sort": lazy(() => import("../algorithms/SelectionSort/SelectionSort.jsx")),
-  "Insertion Sort": lazy(() => import("../algorithms/InsertionSort/InsertionSort.jsx")),
-
+  "Selection Sort": lazy(() =>
+    import("../algorithms/SelectionSort/SelectionSort.jsx")
+  ),
+  "Insertion Sort": lazy(() =>
+    import("../algorithms/InsertionSort/InsertionSort.jsx")
+  ),
+  "Quick Sort": lazy(() => import("../algorithms/QuickSort/QuickSort.jsx")),
 };
 
 export default algoMapping;
